d54b640bed20d7e4b7f043c0d956aa8fdacfcbf9
sakshi <sakshikasera10@gmail.com>
2024-11-17 20:04:37 +0530

    correcting-test-cases

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const request = require('supertest'); // Import supertest
 const app = require('../src/app'); // Import the app, not the server
 
 let server;
@@ -20,7 +21,8 @@ afterAll((done) => {
 
 describe('API Endpoints', () => {
   it('should return a welcome message', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/'); // Use supertest to make a request
     expect(response.text).toBe('Welcome to the server!');
+    expect(response.statusCode).toBe(200); // Optionally, check the status code as well
   });
 });
